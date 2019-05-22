875e0bfbac2f5ccd0b0426ce09c9dd8cde7484b0
João Victor do DS <50925497+NedStark1DS2@users.noreply.github.com>
2019-05-22 15:12:07 -0300

    Adicionado o program.cs

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BizzBuzz
+            double numdig, m3, m5, m35;
+            Console.Write("Digite um número ae: ");
+            numdig = double.Parse(Console.ReadLine());
+            for (int i = 1; i <= numdig; i++ ) 
+            {
+                m3 = i % 3;
+                m5 = i % 5;
+                m35 = m3 + m5;
+                if (m35 == 0)
+                {
+                    Console.WriteLine("BizzBuzz");
+                }
+                else if (m3 == 0)
+                {
+                    Console.WriteLine("Bizz");
+                }
+                else if (m5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
